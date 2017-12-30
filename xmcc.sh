89575aee2e1bc1a48de483103ec238f6a0db99da
DESKTOP-MINSEOK\Minseok <glaxer95@gmail.com>
2017-12-31 01:10:55 +0900

    updated 2.0

--- xmcc.sh
+++ xmcc.sh
@@ -1,22 +1,68 @@
 #!/bin/bash
 
+VERSION=0.12.2
+FILE_RELEASE=monoeciCore-${VERSION}-linux64-cli.Ubuntu16.04.tar.gz
+URL_RELEASE=https://github.com/monacocoin-net/monoeci-core/releases/download/${VERSION}/${FILE_RELEASE}
+
+function xmcc_dependency() {
+	sudo apt-get update
+	sudo apt-get -y install unzip
+	sudo apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libboost-all-dev unzip libminiupnpc-dev python-virtualenv
+	sudo apt-get -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+	sudo apt-get -y install software-properties-common && add-apt-repository ppa:bitcoin/bitcoin
+	sudo apt-get update
+	sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
+	sudo apt-get -y install libzmq3-dev
+}
 
 function xmcc_install() {
+	xmcc_dependency
+	echo "$(tput setaf 1)[INSTALL] XMCC MN $(tput sgr0)"		
 	pushd ~/
-		wget 'https://github.com/monacocoin-net/monacoCoin-Core/releases/download/0.12.1.5.1/monacoCoinCore-0.12.1.5.1-linux64-cli.taz.gz'
-		tar xzvf monacoCoinCore-0.12.1.5.1-linux64-cli.taz.gz
-		chmod +x monacoCoind monacoCoin-tx monacoCoin-cli
+		wget ${URL_RELEASE}
+		tar xzvf ${FILE_RELEASE} ./monoeci
+		chmod +x monoecid monoeci-tx monoeci-cli
+	popd
+	xmcc_run
+	xmcc_sentinel
+	echo "$(tput setaf 1)[INSTALL] XMCC MN Complete! $(tput sgr0)"
+}
+
+function xmcc_sentinel() {
+	pushd ~/.monoeciCore
+		git clone https://github.com/monacocoin-net/sentinel.git
+		pushd sentinel
+			virtualenv ./venv
+			./venv/bin/pip install -r requirements.txt
+		popd
 	popd
 }
 
 function xmcc_run() {
-    pushd ~/
-    	./monacoCoind
+	echo "$(tput setaf 1)[RUN] XMCC $(tput sgr0)"		
+    pushd ~/monoeci/
+    	./monoecid
     popd
 }
 
+function xmcc_stop() {
+	echo "$(tput setaf 1)[STOP] XMCC Wallet$(tput sgr0)"	
+	pkill monoecid
+}
+
+function xmcc_update() {
+	xmcc_stop
+	git pull
+	xmcc_install
+	xmcc_run
+}
+
+function xmcc_conf() {
+	echo "~/.monoeciCore/monoeci.conf"
+}
 
 COMMAND=$1
+
 
 if [ "${COMMAND}" = "install" ]; then 
 	xmcc_install
@@ -24,4 +70,12 @@ fi
 
 if [ "${COMMAND}" = "run" ]; then 
 	xmcc_run
+fi
+
+if [ "${COMMAND}" = "stop" ]; then 
+	xmcc_stop
+fi
+
+if [ "${COMMAND}" = "update" ]; then 
+	xmcc_update
 fi
